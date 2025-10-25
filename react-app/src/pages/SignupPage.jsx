a19c267814a741fa67267edb365f2bb2beff5a31
devyalchemist <devyalchemist@gmail.com>
2025-10-25 21:38:46 +0100

    another slash found in the sign up page

--- react-app/src/pages/SignupPage.jsx
+++ react-app/src/pages/SignupPage.jsx
@@ -37,7 +37,7 @@ export default function SignupPage() {
 					aria-label="Go back">
 					<HiArrowLeft size={28} />
 				</button>
-				\{" "}
+
 				<div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
 					<div className="text-center">
 						<h1 className="text-3xl font-bold text-green-950">
